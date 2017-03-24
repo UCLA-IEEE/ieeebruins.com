e2af26674f599afbac01c929ab5ac5d145e33669
Jeffrey Chan <jeffschan97@gmail.com>
2017-03-24 09:47:05 -0700

    started php

--- index.php
+++ index.php
@@ -6,13 +6,13 @@
     
     <!-- BOOTSTRAP -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     
     <!-- STYLESHEETS -->
     <link rel='stylesheet' href='./css/index.css' />
 </head>
 <body>
+    <?php echo 'MY FIRST PHP SCRIPT' ?>
     <p>Home, About (includes photo gallery), Projects (includes project gallery), IEEE Lab, Sponsors</p>
     <div>
         <p>IEEE at UCLA</p>
