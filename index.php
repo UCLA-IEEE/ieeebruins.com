6e1b3c282f31429aaa1e475c4af43154f2a46ce7
Jeffrey Chan <jeffschan97@gmail.com>
2017-04-04 09:29:45 -0700

    fleshed out footer into something presentable

--- index.php
+++ index.php
@@ -3,13 +3,18 @@
 <head>
     <title>IEEE at UCLA</title>
     <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     
     <!-- BOOTSTRAP -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     
+    <!-- FONT AWESOME -->
+    <script src="https://use.fontawesome.com/348865e609.js"></script>
+    
     <!-- STYLESHEETS -->
     <link rel='stylesheet' href='./css/index.css' />
+    <link rel='stylesheet' href='./css/footer.css' />
 </head>
 <body>
     <?php include('./includes/navbar.html'); ?>
