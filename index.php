46b51f0f8fb3af7456dd7062d5d87869ede14c03
Jeffrey Chan <jeffschan97@gmail.com>
2017-04-14 10:26:29 -0700

    navbar fixed top and transitions color when you scroll

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     
-    <!-- jQuery -->
+    <!-- JQUERY -->
     <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
     
     <!-- BOOTSTRAP -->
