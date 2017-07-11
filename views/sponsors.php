d20b2447557d3f5076160bf397385d8c0b0ddbcf
Jordan Yen <jordanyen726@gmail.com>
2017-07-10 22:56:31 -0700

    more text and pictures

---
+++ views/sponsors.php
@@ -0,0 +1,82 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title> Sponsors </title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- jQuery -->
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+
+    <!-- BootStrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    <!-- FONT AWESOME -->
+    <script src="https://use.fontawesome.com/348865e609.js"></script>
+
+    <!-- CSS Pages -->
+    <link rel="stylesheet" type="text/css" href="../public/css/footer.css" />
+    <link rel="stylesheet" type="text/css" href="../public/css/navbar.css" />
+    <link rel="stylesheet" type="text/css" href="../public/css/sponsors.css">
+</head>
+    
+<body>
+    <!-- Navbar -->
+    <?php include('../views/includes/navbar.html'); ?>
+    <!-- Footer -->
+    <?php include('../views/includes/footer.html'); ?>
+    
+    <div class="container">
+        <h3> Benefits of Becoming a Sponsor</h3>
+        <ul>
+            <li>Priority status and multiple events (info-sessions, tech-talks, etc.) per year (as per package)</li>
+            <li>Priority access to award winning event organization, management and publicity</li>
+            <li>Advertising your job/internship openings through us</li>
+            <li>Reaching out to 1000+ mailing list members and 400+ club members for publicity</li>
+            <li>Getting involved with one of the largest engineering student groups in UCLA</li>
+            <li>Being advertised at various state-wide engineering competitions</li>
+            <li>Being a generous contributor to the development of young engineers</li>
+            <li>Getting all this for free! <strong>Donations to us are tax-deductible</strong></li>
+        </ul>
+        <h3> Becoming a Sponsor</h3>
+        <h4> There are four levels of donations (materials and equipments are also appreciated and are weighed on their monetary value): </h4>
+        <h4>
+            Level 1 (Silver) $500 – $999 <br>
+            Level 2 (Gold) $1000 – $1999 <br>
+            Level 3 (Platinum) $2000 – $4999 <br>
+            Level 4 (Diamond) $5000 and above
+        </h4>
+    </div>
+    
+    <div class="container" id="platinum">
+        <h1> Sponsors </h1>
+        <hr>
+        
+        <div class="row">
+            <div class="col-md-4">
+                <img style="width: 100%; padding-top: 20px;" src="../public/images/company-logos/ee-dept.png">
+            </div>
+            <div class="col-md-4">
+                <img style="width: 100%; padding-top: 50px;" src="../public/images/company-logos/northrop-grumman-logo.jpg">
+            </div>
+            <div class="col-md-4" >
+                <img style="width: 90%; padding-top: 35px;" src="../public/images/company-logos/clas.jpg">
+            </div>
+        </div>
+    </div>
+    <!--Platinum Tier -->
+    
+    <!--ucla engineering, northrop grunman, ieee coastal, boeing, ieee university, edison -->
+
+    <!--Silver Tier -->
+    
+    <!-- reeves, shelving, tappers, steinberg, marlees, inkaddict, wallside, homefirst, bittele, reverie, credibly, greek own, vivid seats -->
+
+    <!--Contact  -->
+    <h1> Interested in Sponsoring? </h1>
+    <p> Fill out this form! </p>
+    <p> View and download our sponsorship brochure <a href="../public/files/SponsorshipBrochure2017.pdf" target="_blank">here!</a> </p>
+    <p> Contact our current president at <a href="president@ieee.ucla.edu" target="_blank">president@ieee.ucla.edu</a> for more infomration.</p>
+</body>
+</html>
