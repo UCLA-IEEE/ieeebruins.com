d993ca39eee4888bf1b9ad7652cbaf850ed0f8b8
Nihar Tamhankar <nihartamhankar@Nihars-MBP.attlocal.net>
2017-07-14 00:23:30 -0700

    Stuff added to countdown page

--- views/countdown.php
+++ views/countdown.php
@@ -1,1 +1,40 @@
-<p>Countdown<p>
+<!DOCTYPE HTML>
+
+<body>
+
+ <div class="topleft">
+    <p>UCLA IEEE</p>
+  </div>
+ <div class="middle">
+    <h1>COMING SOON</h1>
+
+
+</body>
+
+
+
+
+<p id="demo"></p>
+
+<script>
+var countDownDate = new Date("July 31, 2017 19:00:00").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+ 
+  var distance = countDownDate - now;
+
+ 
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  
+}, 1000);
+</script>
