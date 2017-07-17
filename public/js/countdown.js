05e97549f2f51839e874f337c5fb06b73708c97c
Jeffrey Chan <jeffschan97@gmail.com>
2017-07-17 00:53:12 -0700

    Minor code styling changes to clean up code. Moved countdown to root so that it'll display as front page

--- public/js/countdown.js
+++ public/js/countdown.js
@@ -1,36 +1,21 @@
+function addLeadingZero(num) {
+  if (num < 10)
+    return '0' + num
+  return num
+}
 
 let countDownDate = new Date("July 31, 2017 19:00:00").getTime();
 
 let x = setInterval(function() {
 
   let now = new Date().getTime()
-
- 
   let distance = countDownDate - now
 
- 
-  let days = Math.floor(distance / (1000 * 60 * 60 * 24))
-  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
-  let seconds = Math.floor((distance % (1000 * 60)) / 1000)
+  let days = addLeadingZero(Math.floor(distance / (1000 * 60 * 60 * 24)))
+  let hours = addLeadingZero(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)))
+  let minutes = addLeadingZero(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)))
+  let seconds = addLeadingZero(Math.floor((distance % (1000 * 60)) / 1000))
 
-	if(days<10){
-		days="0"+days
-	}
-	if(hours<10){
-		hours="0"+hours
-	}
-	if(minutes<10){
-		minutes="0"+minutes
-	}
-	if(seconds<10){
-		seconds="0"+seconds
-	}
+  $('.countdown').html(days + ':' + hours + ':' + minutes + ':' + seconds)
 
-	$('.countdown').html(days + ":" + hours + ":"
-  	+ minutes + ":" + seconds)
-
-
-  
-  
 }, 1000)
