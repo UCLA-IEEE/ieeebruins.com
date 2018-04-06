6ec8a4406f02689847c86d54dc65bca11558f2ef
Michelle Tan <michellektan>
2018-04-06 00:16:10 -0700

    sponsors page

--- views/sponsors.php
+++ views/sponsors.php
@@ -17,6 +17,8 @@
 
     <!-- CSS Pages -->
     <link rel="stylesheet" type="text/css" href="/public/css/style.css">
+        <link rel="stylesheet" type="text/css" href="/public/css/sponsors.css">
+
     <link rel="stylesheet" type="text/css" href="/public/css/footer.css">
     <link rel="stylesheet" type="text/css" href="/public/css/navbar.css">
 
@@ -27,6 +29,124 @@
     <!-- Navbar -->
     <?php include('../views/includes/navbar.html'); ?>
 
+    <!--Body -->
+    <div class = "allcontent">
+ <h1 align = "center" style = "margin-top: 50px">
+  <span class="black">OUR</span>
+  <span class="ieee-blue">SPONSORS</span>
+</h1>
+<div class = "allsponsors text-center">
+<div class = "sponsorsbox">
+    <div class = "sponsorimages">
+    <a href = "http://www.ee.ucla.edu" target = "_blank">
+        <img style = "width: 340px; margin: 20px 20px;" src = "/public/images/company-logos/ee-dept.png">
+    </a>
+  <a href="http://www.northrupgrumman.com" target="_blank">
+                <img style="width: 320px; margin: 20px 20px;" src="../public/images/company-logos/northrop-grumman-logo.jpg">
+            </a>
+            <a href="http://www.ieee-lac.org/category/coastal-los-angeles-section/" target="_blank">
+                <img style="width: 220px; margin: 20px 20px;" src="../public/images/company-logos/clas.jpg">
+            </a>
+            <a href="http://www.boeing.com" target="_blank">
+                <img style="width: 250px; margin: 20px 20px;" src="../public/images/company-logos/boeing.jpg">
+            </a>
+            <img style="width: 250px; padding: 35px 10px;" src="../public/images/company-logos/ieee-upp.png">
+            <a href="http://on.sce.com/1jMB5Gu" target="_blank">
+                <img style="width: 280px; margin: 20px 20px;" src="../public/images/company-logos/socal-edison.svg">
+            </a>
+        </div>
+<div class = "tiersbox" id = "label"><h1>PLATINUM</h1></div>
+</div>
+<div class = "sponsorsbox">
+    <div class = "sponsorsimages">
+    <a href="http://losangeles.robertreeveslaw.com" target="_blank">
+                <img style="width: 200px; margin: 20px 20px;" src="../public/images/company-logos/reeves-law.jpg">
+            </a>
+            <a href="http://www.shelving.com/" target="_blank">
+                <img style="width: 190px; margin: 20px 20px;" src="../public/images/company-logos/shelving.jpg">
+            </a>
+            <a href="http://www.tappers.com/" target="_blank">
+                <img style="width: 170px; margin: 20px 20px;" src="../public/images/company-logos/tappers.png">
+            </a>
+            <a href="http://www.1800leefree.com/" target="_blank">
+                <img style="width: 180px; margin: 20px 20px;" src="../public/images/company-logos/leeSteinberg.jpg">
+            </a>
+            <a href="http://marleesstyle.com/" target="_blank">
+                <img style="width: 150px; margin: 20px 20px;" src="../public/images/company-logos/marlees-tappers.jpg">
+            </a>
+            <a href="http://www.inkaddict.com/" target="_blank">
+                <img style="width: 160px; margin: 20px 20px;" src="../public/images/company-logos/inkaddict.png">
+            </a>
+            <a href="http://www.wallsidewindows.com/" target="_blank">
+                <img style="width: 100px; margin: 20px 20px;" src="../public/images/company-logos/wallside.png">
+            </a>
+            <a href="http://homefirstcertified.com" target="_blank">
+                <img style="width: 130px; margin: 20px 20px;" src="../public/images/company-logos/homefirst.png">
+            </a>
+            <a href="http://www.7pcb.com/" target="_blank">
+                <img style="width: 160px; margin: 20px 20px;" src="../public/images/company-logos/bittele-logo.png">
+            </a>
+            <a href="http://www.reverie.com/" target="_blank">
+                <img style="width: 100px; margin: 20px 20px;" src="../public/images/company-logos/reverie.jpg">
+            </a>
+            <a href="https://credibly.com/" target="_blank">
+                <img style="width: 200px; margin: 20px 20px;" src="../public/images/company-logos/credibly.jpg">
+            </a>
+            <a href="https://www.greektowncasino.com/" target="_blank">
+                <img style="width: 140px; margin: 20px 20px;" src="../public/images/company-logos/greektown.png">
+            </a>
+            <a href="https://www.vividseats.com/" target="_blank">
+                <img style="width: 170px; margin: 20px 20px;" src="../public/images/company-logos/vivid-seats-logo.png">
+            </a>
+        </div>
+        <div class = "tiersbox" id = "label"><h1>SILVER</h1></div>
+</div>
+</div>
+<h1 id = "titles" align = "left" style = "margin-top: 30px; margin-bottom: 20px">
+  <span class="black">BENEFITS OF</span>
+  <span class="ieee-blue">SPONSORING</span>
+</h1>
+<ul>
+    <li>Priority status and multiple events (info-sessions, tech-talks, etc.) per year (as per package)</li>
+   <li>Priority access to award winning event organization, management and publicity</li>
+   <li>Advertising your job/internship openings through us</li>
+   <li>Reaching out to 1000+ mailing list members and 400+ club members for publicity</li>
+   <li>Getting involved with one of the largest engineering student groups in UCLA</li>
+   <li>Being advertised at various state-wide engineering competitions</li>
+   <li>Being a generous contributor to the development of young engineers</li>
+   <li>Getting all this for free! <b>Donations to us are tax-deductible</b></li>
+    </ul>
+    <br>
+<h1 id = "titles" align = "left" style = "margin-top: 40px; margin-bottom: 20px">
+  <span class="black">BECOMING A</span>
+  <span class="ieee-blue">SPONSOR</span>
+</h1>
+    <div class = "tierscontainer">
+        <div class = "tiersbox" style = "float: left">
+            <h1 id = "tier">SILVER</h1>
+            <p id = "price">$500 - $999</p>
+        </div> 
+        <div class = "tiersbox" style = "float: left">
+            <h1 id = "tier">GOLD</h1>
+            <p id = "price">$1000 - $1999</p>
+        </div>
+        <div class = "tiersbox" style = "float: left">
+            <h1 id = "tier">PLATINUM</h1>
+            <p id = "price">$2000 - $4999</p>
+        </div>
+        <div class = "tiersbox" style = "float: left">
+            <h1 id = "tier">DIAMOND</h1>
+            <p id = "price">$5000</p>
+        </div>
+    </div>
+<div class = "interestbox">
+<p style = "line-height: 29px">
+    Fill out this <a href = "http://new.ieeebruins.com/public/files/SponsorshipForm2017.pdf" target = "_blank">form</a>!<br>
+    View and download our sponsorship brochure <a href = http://new.ieeebruins.com/public/files/SponsorshipBrochure2017.pdf target = "_blank">here</a>!<br>
+    Contact our current club president at <a href = "mailto:president@ieee.ucla.edu">president@ieee.ucla.edu </a> for more information.
+</p>
+</div>
+</div>
     <!-- Footer -->
     <?php include('../views/includes/footer.html'); ?>
 </body>
