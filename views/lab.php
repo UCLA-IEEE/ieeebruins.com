42d22f32d9a87ab2ff42c4e5ac268ef0026ed90f
Anirudh Balasubramaniam <anirudhnkl@gmail.com>
2018-04-05 21:02:52 -0700

    m

--- views/lab.php
+++ views/lab.php
@@ -1,19 +1,43 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>IEEE Lab - IEEE at UCLA</title>
-    <link rel="stylesheet" href="labstyle.css" />
+    <title>IEEE at UCLA</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- JQUERY -->
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+
+    <!-- BOOTSTRAP -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    <!-- FONT AWESOME -->
+    <script src="https://use.fontawesome.com/348865e609.js"></script>
+
+    <!-- STYLESHEETS -->
+    <link rel='stylesheet' href='/public/css/lab.css' />
+    <link rel='stylesheet' href='/public/css/footer.css' />
+    <link re-='stylesheet' href='/public/css/style.css' />
+    <link rel='stylesheet' href='/public/css/navbar.css' />
+
+    <!--Favicon -->
+    <link rel='icon' href='/public/favicon.ico'/>
 </head>
+<!-- NAVBAR -->
+<?php include('./includes/navbar.html'); ?>
 <body>
-    <div id="header">
-        <h1>Welcome to the IEEE Lab!</h1>
+
+    <div class='info-page'>
+        <h1>IEEE LAB</h1>
     </div>
-    <div id="map">
-        <p id="directions">Our lab is located at Boelter 2825. Come find us! The easiest way to find it is to go to the west side of 2nd floor Boelter (the lowest balcony). We are the second door down from the bridge to E-IV. The door’s usually propped open using a small wooden block. Also, the IEEE logo is posted on our door for easy locating.</p>
+     
+    <div class='lab-hours'>
+
     </div>
-    <a href="https://docs.google.com/forms/d/1QL2kGazOb2a2CL0P63MoZs1_IpwAg6uoar4aClZsEoA/viewform?edit_requested=true"> Click here to sign up for 3d printer and CNC Mill</a>
-    <div id="partOrders">
-        <p>How to Order Parts <br /> We know ordering custom parts can be difficult. Here’s a quick guideline you can follow so you can order parts more efficiently! <br /> Check to see if the IEEE lab has any of the parts you are looking for. Our lab is always stocked with circuit components! Just ask our Projects Manager, Julian, or any other officer member if you need assistance. <br /> Search online using Digikey or Mouser Electronics. These are the two main vendors IEEE at UCLA purchases from. So if you can find your parts from these two sites, it will make ordering a lot easier. <br /> If you can’t find the parts on the websites mentioned above, take a look at SparkFun Electronics and All Electronics. <br /> Lastly, if you still can’t come across the parts you need, ask your projects lead for other recommendations. <br /> IEEE at UCLA Projects is aiming to make it hassle-free for you and the officer members when ordering parts. If you have any questions feel free to contact any IEEE officer member and we’ll be glad to help you.</p>
+
+    <div class='resources'>
+    
     </div>
 </body>
 </html>
