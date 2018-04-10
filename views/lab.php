5ce63aabbdf73fb936f70218756a72417b7a2e44
Anirudh Balasubramaniam <anirudhnkl@gmail.com>
2018-04-09 17:07:40 -0700

    first update

--- views/lab.php
+++ views/lab.php
@@ -1,7 +1,6 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<<<<<<< HEAD
     <title>IEEE at UCLA</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -17,35 +16,38 @@
     <script src="https://use.fontawesome.com/348865e609.js"></script>
 
     <!-- STYLESHEETS -->
+    <link rel='stylesheet' href='/public/css/style.css' />
     <link rel='stylesheet' href='/public/css/lab.css' />
     <link rel='stylesheet' href='/public/css/footer.css' />
-    <link re-='stylesheet' href='/public/css/style.css' />
     <link rel='stylesheet' href='/public/css/navbar.css' />
 
-    <!--Favicon -->
+    <!--Favicon --> 
     <link rel='icon' href='/public/favicon.ico'/>
-=======
-    <title>Lab - IEEE at UCLA</title>
->>>>>>> master
 </head>
 <!-- NAVBAR -->
 <?php include('./includes/navbar.html'); ?>
 <body>
-<<<<<<< HEAD
 
     <div class='info-page'>
-        <h1>IEEE LAB</h1>
+        <h1>THE IEEE </h1><h1 class='ieee-blue'>LAB</h1>
+        <hr style='width: 210px;'>
+        <p class='details'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non</p>
+        <div class='lab-map'>
+            <img style="float:center; max-width:100%; padding=0; margin=0;" src="/public/images/lab/map.png">
+        </div>
     </div>
      
-    <div class='lab-hours'>
-
+    <div class="background-grey lab-hours ">
+        <h1 class='ieee-blue'>LAB HOURS</h1>
+        <hr style='width: 330px;'>
+        <div class='lab-hours-table' style="height: 500px;"></div>
     </div>
 
     <div class='resources'>
-    
+        <h1 class='black'>RESOURCES</h1>
+        <hr style='width: 310px; margin-bottom: 10px;'>
+        <p class='black'>CREATIVITY HAS NO LIMITS</p>
     </div>
-=======
-    <p>This is the lab page</p>
->>>>>>> master
 </body>
+<?php include('./includes/footer.html'); ?>
 </html>
