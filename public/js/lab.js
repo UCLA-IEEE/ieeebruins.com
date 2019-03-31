f5a8ff1956e2a98757f00f944904e0beebbf926b
Kathy Daniels <danielskathyd@gmail.com>
2019-03-31 16:14:51 -0700

    Updating spring quarter lab hours

--- public/js/lab.js
+++ public/js/lab.js
@@ -1,6 +1,6 @@
 const SPREADSHEET_ID = '1xzxbdPNLEIT31dClA8FAZF3MMCsmEBwzhZOfp5eHEVc'
 const API_BASE_URL = 'https://sheets.googleapis.com/v4/spreadsheets/'
-const API_OPTIONS = '/values/Winter%202019!A1:G11?key='
+const API_OPTIONS = '/values/Spring%202019!A1:G11?key='
 // API_KEY provided in separate file key.js so that we don't have to push
 // it to GitHub
 
