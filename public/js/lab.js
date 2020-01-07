dcc5a1515eaa2fa171a01f280022d34942e5068a
Kathy Daniels <danielskathyd@gmail.com>
2020-01-06 23:55:15 -0800

    update winter 2020 lab hours

--- public/js/lab.js
+++ public/js/lab.js
@@ -1,6 +1,6 @@
 const SPREADSHEET_ID = '1785xI9FY9gNxuSC0j6njYueiac1-w1-fL81eruj1STY'
 const API_BASE_URL = 'https://sheets.googleapis.com/v4/spreadsheets/'
-const API_OPTIONS = '/values/Fall%202019!A1:G11?key='
+const API_OPTIONS = '/values/Winter%202020!A1:G11?key='
 // API_KEY provided in separate file key.js so that we don't have to push
 // it to GitHub
 
