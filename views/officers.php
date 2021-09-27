5668fc300a5848d26784056363edfd879d1976b5
robertnperalta <47515219+robertnperalta@users.noreply.github.com>
2021-09-27 08:41:34 -0700

    Update officers.css cache busting

--- views/officers.php
+++ views/officers.php
@@ -248,7 +248,7 @@
     <link rel='stylesheet' href='/public/css/footer.css?v=1.0.0' />
     <link rel='stylesheet' href='/public/css/navbar.css?v=1.0.1' />
     <link rel='stylesheet' href='/public/css/style.css?v=1.0.0' />
-    <link rel='stylesheet' href='/public/css/officers.css?v=1.0.0?v=1.0.0' />
+    <link rel='stylesheet' href='/public/css/officers.css?v=1.0.1' />
 
     <!-- SCRIPTS -->
     <script src="/public/js/officers.js"></script>
