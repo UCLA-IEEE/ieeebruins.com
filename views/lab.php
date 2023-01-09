fa0ea3def34b4b469cf955bbef4a80b074aff729
Paul Zhang <paulz@ucla.edu>
2023-01-09 13:21:03 -0800

    Fix cache busting for Lab Hours script

--- views/lab.php
+++ views/lab.php
@@ -23,7 +23,7 @@
 
     <!-- SCRIPTS -->
     <script src='/public/js/key.js'></script>
-    <script src="/public/js/lab.js?v=W23.1"></script>
+    <script src="/public/js/lab.js?v=W23.0"></script>
 
     <!--Favicon -->
     <link rel='icon' href='/public/favicon.ico' />
