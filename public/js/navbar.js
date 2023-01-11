d1c427b3c02e218d0f28ac4dec987785961bd095
Paul Zhang <paulz@ucla.edu>
2023-01-11 14:17:31 -0800

    Apply overhauled formatting and linting
    - Manually fix some lint errors

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,32 +1,32 @@
 // function to handle logic when navbar icon pressed
 // eslint-disable-next-line no-unused-vars
 function toggleNavbarIconLogic(navbarIcon) {
-  navbarIcon.classList.toggle('change')
-  toggleMobileNav()
+  navbarIcon.classList.toggle("change");
+  toggleMobileNav();
 }
 
 function toggleMobileNav() {
-  var idText = 'mobile-nav-list'
-  if (document.getElementById(idText).style.height === 'auto') {
-    document.getElementById(idText).style.height = '0%'
+  var idText = "mobile-nav-list";
+  if (document.getElementById(idText).style.height === "auto") {
+    document.getElementById(idText).style.height = "0%";
   } else {
-    document.getElementById(idText).style.height = 'auto'
+    document.getElementById(idText).style.height = "auto";
   }
 }
 
 // eslint-disable-next-line no-unused-vars
 function toggleResponsiveActive() {
-  let x = document.getElementById('responsive-content')
-  if (x.className === 'responsive-dropdown-content') {
-    x.className += ' active'
+  const x = document.getElementById("responsive-content");
+  if (x.className === "responsive-dropdown-content") {
+    x.className += " active";
   } else {
-    x.className = 'responsive-dropdown-content'
+    x.className = "responsive-dropdown-content";
   }
 
-  let dropdownArrow = document.getElementById('dropdown-arrow')
-  if (dropdownArrow.style.transform === 'rotate(180deg)') {
-    dropdownArrow.style.transform = 'none'
+  const dropdownArrow = document.getElementById("dropdown-arrow");
+  if (dropdownArrow.style.transform === "rotate(180deg)") {
+    dropdownArrow.style.transform = "none";
   } else {
-    dropdownArrow.style.transform = 'rotate(180deg)'
+    dropdownArrow.style.transform = "rotate(180deg)";
   }
 }
--- views/error/403.php
+++ views/error/403.php
@@ -1,4 +1,4 @@
 <?php
-    $error_type = "403";
-    $error_text = "403 Permission Denied";
-    require_once('./error-template.php');
+$error_type = "403";
+$error_text = "403 Permission Denied";
+require_once "./error-template.php";
