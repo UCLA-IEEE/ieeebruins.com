5704fb063384bb49759a18b01fa4a83c955b8a18
Paul Z <paulz@ucla.edu>
2023-03-15 15:22:25 -0700

    Workshops page (#85)
    * Prototype workshops page
    
    * Add complete workshops page

---
+++ views/outreach.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Outreach - IEEE at UCLA</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- JQUERY -->
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+
+    <!-- BOOTSTRAP -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    <!-- FONT AWESOME -->
+    <script src="https://kit.fontawesome.com/2fb39e755e.js" crossorigin="anonymous"></script>
+
+    <!--Favicon -->
+    <link rel='icon' href='/public/favicon.ico' />
+
+    <!-- STYLESHEETS -->
+    <link rel='stylesheet' href='/public/css/footer.css?v=1.0.0' />
+    <link rel='stylesheet' href='/public/css/navbar.css?v=1.0.1' />
+    <link rel='stylesheet' href='/public/css/style.css?v=1.0.0' />
+
+    <!-- SCRIPTS -->
+    <script src="/public/js/about.js"></script>
+
+</head>
+<body>
+    <!-- NAVBAR -->
+    <?php include "./includes/navbar.html"; ?>
+
+    <!-- DESCRIPTION -->
+    <div class="background-ieee-blue">
+        <h1>OUTREACH</h1>
+    </div>
+</body>
+</html>
---
+++ views/workshops.php
@@ -0,0 +1,391 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Workshops - IEEE at UCLA</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- JQUERY -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <!-- BOOTSTRAP -->
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+      integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+      crossorigin="anonymous"
+    />
+    <script
+      src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+      integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+      crossorigin="anonymous"
+    ></script>
+
+    <!-- FONT AWESOME -->
+    <script
+      src="https://kit.fontawesome.com/2fb39e755e.js"
+      crossorigin="anonymous"
+    ></script>
+
+    <!-- STYLESHEETS -->
+    <link rel="stylesheet" href="/public/css/footer.css?v=1.0.0" />
+    <link rel="stylesheet" href="/public/css/navbar.css?v=1.0.1" />
+    <link rel="stylesheet" href="/public/css/workshops.css?v=1.0.0" />
+    <link rel="stylesheet" href="/public/css/style.css?v=1.0.0" />
+
+    <!--Favicon -->
+    <link rel="icon" href="/public/favicon.ico" />
+  </head>
+  <body>
+    <!-- NAVBAR -->
+    <?php include "./includes/navbar.html"; ?>
+    <div class="pt-3"></div>
+
+    <!--Page Title-->
+    <div
+      class="container-fluid text-center page-title content-wrapper"
+      style="margin-top: 50px"
+    >
+      <h1>Our <span class="ieee-blue">Workshops</span></h1>
+      <p>
+        For members who do not have time to dedicate a whole year for one of our
+        competition projects, this program is for you. IEEE holds a series of
+        workshops throughout the school year where anyone can participate and
+        learn about new skills and technologies. These projects are aimed at
+        teaching everyone, engineers and non-engineers, about common skills in
+        electrical engineering such as soldering, image processing, prototyping,
+        and electronics. To receive more information on upcoming IEEE workshops,
+        subscribe to our
+        <a href="https://uclaieeenewsletter.substack.com/">newsletter</a>.
+      </p>
+    </div>
+
+    <div class="container-fluid ops background-ieee-blue">
+      <div class="content-wrapper">
+        <div class="row flex-container">
+          <div class="col-md-4 header">
+            <h1>Basic Circuit Workshop</h1>
+            <h2>October 6th, 2022</h2>
+          </div>
+
+          <div class="col-md-8 descrip">
+            <p>
+              If you want to get started with electrical engineering, you should
+              first learn a little bit about electricity! We will start from the
+              beginning with how electricity works, basic rules of circuits, and
+              the most common components. Whether resistors, transistors, or
+              LEDs, the behavior of electronic components will be clarified as
+              you work hands-on with partners to build and test simple circuits.
+            </p>
+          </div>
+        </div>
+        <div class="picture-box flex-container">
+          <iframe
+            width="480"
+            height="299"
+            src="https://www.youtube-nocookie.com/embed/5UpfCaU40ls"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+          <iframe
+            src="https://docs.google.com/presentation/d/e/2PACX-1vSPyk5N3Z0ANtVTgBMLYGkNpskvD4nI35q8QsAHH-4NWEYB4ubUHDlKIegEGLR7tO5Z2fCjr2UE8wsk/embed?start=false&loop=false&delayms=3000"
+            frameborder="0"
+            width="480"
+            height="299"
+            allowfullscreen="true"
+            mozallowfullscreen="true"
+            webkitallowfullscreen="true"
+          ></iframe>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid ops background-grey">
+      <div class="content-wrapper">
+        <div class="row flex-container">
+          <div class="col-md-4 header">
+            <h1>Soldering Workshop</h1>
+            <h2>October 20th, 2022</h2>
+          </div>
+
+          <div class="col-md-8 descrip">
+            <p>
+              If you want to have the ability to build your own projects,
+              soldering is one of those essential EE skills that you must have!
+              Join us as we teach you how to solder through-hole and SMD
+              (Surface Mount Device) resistors, LEDs, ICs, and much more! As the
+              hands-on experience for this workshop, you will be making your
+              very own light up IEEE sign!
+            </p>
+          </div>
+        </div>
+        <div class="picture-box flex-container">
+          <iframe
+            width="480"
+            height="299"
+            src="https://www.youtube-nocookie.com/embed/S5WJ6GGtIkg"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+          <iframe
+            src="https://docs.google.com/presentation/d/e/2PACX-1vR13XQnBrCjN33vOPTPCbYXQJGIM8AJPw5tFlBxbIkWogiW3ojFbkXom5DhrrJkGE2SYCe7i9ODy3ed/embed?start=false&loop=false&delayms=3000"
+            frameborder="0"
+            width="480"
+            height="299"
+            allowfullscreen="true"
+            mozallowfullscreen="true"
+            webkitallowfullscreen="true"
+          ></iframe>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid ops background-ieee-blue">
+      <div class="content-wrapper">
+        <div class="row flex-container">
+          <div class="col-md-4 header">
+            <h1>PCB Design Workshop</h1>
+            <h2>November 8th, 2022</h2>
+          </div>
+
+          <div class="col-md-8 descrip">
+            <p>
+              Are you tired of breadboards and messy jumper wires? No worries,
+              we’ve got you covered. Make sure to join our PCB Design workshop,
+              where we will teach you the basics of how to design a functioning
+              PCB.
+            </p>
+          </div>
+        </div>
+        <div class="picture-box flex-container">
+          <iframe
+            width="480"
+            height="299"
+            src="https://www.youtube-nocookie.com/embed/y9sA2SG3twQ"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+          <iframe
+            src="https://docs.google.com/presentation/d/e/2PACX-1vR13XQnBrCjN33vOPTPCbYXQJGIM8AJPw5tFlBxbIkWogiW3ojFbkXom5DhrrJkGE2SYCe7i9ODy3ed/embed?start=false&loop=false&delayms=3000"
+            frameborder="0"
+            width="480"
+            height="299"
+            allowfullscreen="true"
+            mozallowfullscreen="true"
+            webkitallowfullscreen="true"
+          ></iframe>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid ops background-grey">
+      <div class="content-wrapper">
+        <div class="row flex-container">
+          <div class="col-md-4 header">
+            <h1>Resume & Interview Workshop</h1>
+            <h2>November 15th, 2022</h2>
+          </div>
+
+          <div class="col-md-8 descrip">
+            <p>
+              Just having engineering skills isn't enough to land the job, you
+              need to be able to show them. Join us as we teach you to separate
+              the good resumes from the bad, answer those tricky interview
+              questions, and learn about the internship recruitment process.
+              Then, put what you've learned to the test in our resume review and
+              practice interview rooms with older students!
+            </p>
+          </div>
+        </div>
+        <div class="picture-box flex-container">
+          <iframe
+            width="480"
+            height="299"
+            src="https://www.youtube-nocookie.com/embed/9l1WjrBr_hY"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+          <iframe
+            src="https://docs.google.com/presentation/d/e/2PACX-1vRU_Jh3hqUGn0P8wBFMGiC-xiNLr3B7kpYri-Ue1u2MzN3eEjyreawGiZwD7V6dXC3rlApXmtwomsu5/embed?start=false&loop=false&delayms=3000"
+            frameborder="0"
+            width="480"
+            height="299"
+            allowfullscreen="true"
+            mozallowfullscreen="true"
+            webkitallowfullscreen="true"
+          ></iframe>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid ops background-ieee-blue">
+      <div class="content-wrapper">
+        <div class="row flex-container">
+          <div class="col-md-4 header">
+            <h1>Verilog Workshop</h1>
+            <h2>February 10th, 2022</h2>
+          </div>
+
+          <div class="col-md-8 descrip">
+            <p>
+              What is HDL? What is digital logic? What is an FPGA? We got you
+              covered. The Verilog Workshop will teach you anything from binary
+              computation to how to code an FPGA. We cover topics like how to
+              operate binary numbers, logic gates, systemVerilog, and waveforms.
+              In the end you will be able to complete a decoder module on EDA
+              playground.
+            </p>
+          </div>
+        </div>
+        <div class="picture-box flex-container">
+          <iframe
+            width="480"
+            height="299"
+            src="https://www.youtube-nocookie.com/embed/1ZhrkpkhiWw"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+          <iframe
+            src="https://docs.google.com/presentation/d/e/2PACX-1vS0lsEWC-nMdIGY0rFs_rt3pvyMKac5k32FMl-Y6YruIA8dHVQlDGWfLlHcnbZMrrwy2s2anm2Zk7QX/embed?start=false&loop=false&delayms=3000"
+            frameborder="0"
+            width="480"
+            height="299"
+            allowfullscreen="true"
+            mozallowfullscreen="true"
+            webkitallowfullscreen="true"
+          ></iframe>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid ops background-ieee-grey">
+      <div class="content-wrapper">
+        <div class="row flex-container">
+          <div class="col-md-4 header">
+            <h1>3D Printing Workshop</h1>
+            <h2>October 20th, 2020</h2>
+          </div>
+
+          <div class="col-md-8 descrip">
+            <p>
+              Learn the in's and out's of 3D printing, and start your own print!
+              Ever wonder how these printers work? Whether you'd like to print
+              your own trinkets, food, or rocket ships, 3D printing has taken
+              the world by storm. Come by to get an introduction to how 3D
+              printing works and what makes a successful print, as well as how
+              to get it done on your own! We'll prove to you, once and for all,
+              that 3D is better than 2D.
+            </p>
+          </div>
+        </div>
+        <div class="picture-box flex-container">
+          <iframe
+            width="480"
+            height="299"
+            src="https://www.youtube-nocookie.com/embed/F81Y8SCLcN4"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid ops background-ieee-blue">
+      <div class="content-wrapper">
+        <div class="row flex-container">
+          <div class="col-md-4 header">
+            <h1>LTspice Workshop</h1>
+            <h2>May 2nd, 2020</h2>
+          </div>
+
+          <div class="col-md-8 descrip">
+            <p>
+              Come out to an introductory workshop to spice software for circuit
+              simulation. We'll go over the basics using LTspice on how to set
+              circuits up and do dc and ac analysis. Useful for personal
+              projects or even circuits classes!!
+            </p>
+          </div>
+        </div>
+        <div class="picture-box flex-container">
+          <iframe
+            width="480"
+            height="299"
+            src="https://www.youtube-nocookie.com/embed/i7fGLyWqwfE"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+          <iframe
+            src="https://docs.google.com/presentation/d/e/2PACX-1vTzdO5veWOPV0wwDw8mIIph0XzpeGMu-CZPfokbS210hai8wJryea31dK_Sm4TtfCPT495zqbc1ZuM9/embed?start=false&loop=false&delayms=3000"
+            frameborder="0"
+            width="480"
+            height="299"
+            allowfullscreen="true"
+            mozallowfullscreen="true"
+            webkitallowfullscreen="true"
+          ></iframe>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid ops background-ieee-grey">
+      <div class="content-wrapper">
+        <div class="row flex-container">
+          <div class="col-md-4 header">
+            <h1>Advanced PCB Design Workshop</h1>
+            <h2>April 16th, 2020</h2>
+          </div>
+
+          <div class="col-md-8 descrip">
+            <p>
+              This week's workshop will be an Advanced EAGLE session! Join us
+              for a free tutorial on using EAGLE software with more advanced
+              tips on designing PCBs. We'll be going over an in-depth process of
+              common schematic practices, component placement, routing
+              techniques, and more! Up your embedded design game. Make sure to
+              stay safe and workshop managers Evan and Henry hope to see you
+              there! Check out our schedule for more exciting workshops.
+            </p>
+          </div>
+        </div>
+        <div class="picture-box flex-container">
+          <iframe
+            width="480"
+            height="299"
+            src="https://www.youtube-nocookie.com/embed/ElyvoGlYCns"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+          <iframe
+            src="https://docs.google.com/presentation/d/e/2PACX-1vTdW6OaBF1W68aRDHi78sk-boaCDkCNCLBQAqfSVUP5Oh8vNsRIB7g7pdBsvWEiiJMhD40feUngZGWO/embed?start=false&loop=false&delayms=3000"
+            frameborder="0"
+            width="480"
+            height="299"
+            allowfullscreen="true"
+            mozallowfullscreen="true"
+            webkitallowfullscreen="true"
+          ></iframe>
+        </div>
+      </div>
+    </div>
+
+    <!-- FOOTER -->
+    <?php include "./includes/footer.html"; ?>
+  </body>
+</html>
