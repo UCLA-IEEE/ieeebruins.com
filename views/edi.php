adc35efffed564d6a00ae4720b348e1cca17e09b
Paul Zhang <paulz@ucla.edu>
2023-09-29 22:46:35 -0700

    Update Lab Hours spreadsheet for Fall 2023

---
+++ views/edi.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>EDI - IEEE at UCLA</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- JQUERY -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <!-- BOOTSTRAP -->
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+      integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+      crossorigin="anonymous"
+    />
+    <script
+      src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+      integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+      crossorigin="anonymous"
+    ></script>
+
+    <!-- FONT AWESOME -->
+    <script
+      src="https://kit.fontawesome.com/2fb39e755e.js"
+      crossorigin="anonymous"
+    ></script>
+
+    <!--Favicon -->
+    <link rel="icon" href="/public/favicon.ico" />
+
+    <!-- STYLESHEETS -->
+    <link rel="stylesheet" href="/public/css/footer.css?v=1.0.0" />
+    <link rel="stylesheet" href="/public/css/navbar.css?v=1.0.1" />
+    <link rel="stylesheet" href="/public/css/style.css?v=1.0.0" />
+    <link rel="stylesheet" href="/public/css/edi.css?v=1.0.0?v=1.0.0" />
+  </head>
+  <body>
+    <!-- NAVBAR -->
+    <?php include "../views/includes/navbar.html"; ?>
+
+    <!-- CODE OF CONDUCT -->
+
+    <div class="container-fluid competitions text-center">
+      <h1 class="comp-title">
+        Our <span class="ieee-blue">Code of Conduct</span>
+      </h1>
+
+      <div class="comp-wrapper">
+        <div class="flex-wrapper">
+          <div class="code-of-conduct text-left">
+            <p>
+              IEEE at UCLA is committed to upholding equity, diversity, and
+              inclusion. Here is our
+              <a
+                href="https://docs.google.com/document/d/1Z6yylIH9p7UhWdtn85KWZj-D0ZcAWIYodXDFbowBkm0/edit?usp=sharing"
+                target="_blank"
+                >code of conduct</a
+              >:
+            </p>
+            <iframe
+              src="https://docs.google.com/document/d/e/2PACX-1vRMrVNNsZeIUILXd9XWBGKhCaW0HIiNEe9lzwd8q3cGUBxq2Jo7bf1bzfgfW5_oulmD2m64uv5scnaQ/pub?embedded=true"
+            ></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- GET INVOLVED -->
+
+    <div class="involved background-ieee-blue conatiner-fluid text-center">
+      <h1 class="white">Get Involved</h1>
+
+      <div class="involvement-wrapper">
+        <div class="involvement text-left">
+          <h2>IEEE Worldwide Organization</h2>
+          <p>
+            The Institute of Electrical and Electronics Engineers (IEEE) is the
+            primary professional society of the electrical engineering
+            profession. It has more than 330,000 members worldwide; including
+            52,000 student members. It publishes more than 40 technical journals
+            and sponsors or co-sponsors more than 1,000 scientific conferences
+            and meetings around computer engineering, computer science, and
+            related fields. The IEEE has a student branch in every major
+            university in curriculum. Getting involved with the IEEE worldwide
+            organization is highly encouraged, and can provide you with many
+            resources to jump-start an engineering career and expand your
+            network.
+            <br />
+            <br />
+            To find out more, visit
+            <a href="https://www.ieee.org/" target="_blank"
+              >the IEEE website!</a
+            >
+          </p>
+        </div>
+        <div class="involvement text-left">
+          <h2>General Member</h2>
+          <p>
+            One of the most common questions we receive is how can one become a
+            member of our club. We don't have tight restrictions on who is and
+            who isn't a member. Becoming a general member of our club is as
+            simple as getting involved with one of our project curriculums,
+            participating in any of our annual workshop series, showing up to a
+            social or two, joining our Facebook page, or even just sitting in
+            the lab just to say hi to your favorite officers. <br /><br />
+            Whichever you choose to do to get more involved, we'll be more than
+            happy to welcome you to the club!
+          </p>
+        </div>
+        <div class="involvement text-left">
+          <h2>General Board</h2>
+          <p>
+            General Board is our annual mentorship program. Every year, members
+            of our officer board choose to lead a group of 3-5 general members
+            for the rest of the academic year. Each GB decides on a a set of
+            focuses for their group, chosen among technical projects, hosting
+            socials and events, or professional development. In addition,
+            members of a GB get first-hand experience in what it's like to be an
+            officer for this organization, which usually motivates higher club
+            involvement in the future. <br /><br />
+            General Board is a great way to experience a closer and more
+            personal experience with us! Applications and info sessions happen a
+            couple weeks before heading off for Winter break, so keep an eye out
+            for those on our
+            <a href="https://www.facebook.com/groups/uclaieee/" target="_blank"
+              >Facebook page</a
+            >, and in our newsletter.
+            <br />
+            <br />
+            Visit <a href="http://gb.ieeebruins.com/">gb.ieeebruins.com</a>!
+            It's a page we created with some neat visuals and statistics about
+            General Board.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <!-- OUR COMPETITIONS -->
+
+    <!-- FOOTER -->
+    <?php include "../views/includes/footer.html "; ?>
+  </body>
+</html>
