b0ba9313d8b7cc89442020cb5bb8dd783d2923ad
Paul Zhang <paulz@ucla.edu>
2023-11-09 14:27:12 -0800

    Add some officer descriptions

--- views/officers.php
+++ views/officers.php
@@ -17,7 +17,8 @@ $officer_data = [
       "major" => "EE",
       "class" => "3rd Year",
       "picture" => $IMAGE_PATH . "jacqueline.jpg",
-      "description" => "",
+      "description" =>
+        "Hey! My name is Jackie and I'm a 3rd year EE and your internal vice president for this year. Before I was an engineer I was a pre-med, so if you ever want to talk about medicine, I'm not your person but let's still chat :] I love playing piano and recording covers of my fav pop songs with my fav people, drinking boba and coffee, playing tennis, and just going out and spending money. Hmu to hang out <3",
       "pronouns" => "She/Her",
     ],
     "Theo Zhang" => [
@@ -25,7 +26,8 @@ $officer_data = [
       "major" => "CS",
       "class" => "3rd Year",
       "picture" => $IMAGE_PATH . "theo.jpg",
-      "description" => "",
+      "description" =>
+        "Hello, I'm Theo! Aside from IEEE, I like to read, write, and sew. I'm a staff writer for The Westwood Enabler (the satirical newsletter here at UCLA), and I read around 60 books a year!",
       "pronouns" => "They/Them",
     ],
     "Taylor Kawate" => [
@@ -33,7 +35,8 @@ $officer_data = [
       "major" => "EE",
       "class" => "4th Year",
       "picture" => $IMAGE_PATH . "taylor.jpg",
-      "description" => "",
+      "description" =>
+        "Hi I'm Taylor, your External Vice President and previous Corporate Relations Officer! I love playing games, listening to music, and speedrunning. Feel free to reach out to me about anything!",
       "pronouns" => "He/Him",
     ],
     "Miriam Nygren" => [
@@ -52,7 +55,8 @@ $officer_data = [
       "major" => "EE",
       "class" => "3rd Year",
       "picture" => $IMAGE_PATH . "evan.jpg",
-      "description" => "",
+      "description" =>
+        "Hey everyone, I'm Evan the corporate relations officer! I like video games (OW, smash bros, platformers), hiking, and playing music. Always looking for good Mexican and Asian food spots so drop your hidden gems! I have been trying to get more into cooking so am on that youtube recipe grind (Internet Shaquille is the best so far).\n\nDM me on Discord and lets chat!",
       "pronouns" => "He/Him",
     ],
     "Benji Cruz" => [
@@ -68,7 +72,8 @@ $officer_data = [
       "major" => "EE",
       "class" => "2nd Year",
       "picture" => $IMAGE_PATH . "jace.jpg",
-      "description" => "",
+      "description" => "
+      Hellor hellor! I'm Jace, and IEEE's publicity officer this year! I'm the one behind our social media, club merch, and other design-y stuff. When I'm not making posts, catch me bopping to music, playing tennis, and trying my hardest not to nap. See yall around :)",
       "pronouns" => "She/They/He",
     ],
     "Eugenia Cho" => [
@@ -84,7 +89,8 @@ $officer_data = [
       "major" => "EE",
       "class" => "2nd Year",
       "picture" => $IMAGE_PATH . "rachel.jpg",
-      "description" => "",
+      "description" =>
+        "Hellooo my name is Rachel and I'm one of the events coordinators this year! I'm in charge of the non-edible logistics of events like booking rooms and working out the behind-the-scenes details. When I'm not sending a bajillion emails, I'm probably listening to Taylor Swift, taking too many pics and BeReals, or going on a walk :) Feel free to say hi if you see me around!",
       "pronouns" => "She/Her",
     ],
     "Eli Foerst" => [
@@ -92,7 +98,8 @@ $officer_data = [
       "major" => "EE",
       "class" => "3rd Year",
       "picture" => $IMAGE_PATH . "eli.jpg",
-      "description" => "",
+      "description" => "
+      Hi! I'm Eli, a 3rd year EE with heavy interest in RF/Analog Systems! I am currently outreach coordinator so I'm in charge of pushing IEEE at UCLA outside of UCLA, but in the past I was an OPS lead. Feel free to reach out to me to ask about internships, touching grass, circuits, and being on the water.",
       "pronouns" => "He/Him",
     ],
     "Nathan Nguyendinh" => [
@@ -159,7 +166,8 @@ $officer_data = [
       "major" => "CSE",
       "class" => "3rd Year",
       "picture" => $IMAGE_PATH . "claire.jpg",
-      "description" => "",
+      "description" =>
+        "Hi! I'm Claire Huang. I'm a 3rd year computer science and engineering major from the Bay Area with career/academic interests in machine learning, but I prefer to think of myself as a ✨graphic designer✨ (because, after all, graphic design is my passion). This year, I'm one of the DAV leads, which means I spend my time gaslighting, girlbossing, and logic-gatekeeping. In my spare time, I love to draw, bake, and suffer through the trials and tribulations of the Daily Crosswords. If you're interested in any of those, or in chip design or ML, or even if you just wanna talk about why Junimos are the best NPCs in Stardew Valley, feel free to reach out :)",
       "pronouns" => "She/Her",
     ],
     "Premkumar Giridhar" => [
@@ -167,7 +175,8 @@ $officer_data = [
       "major" => "CS",
       "class" => "3rd Year",
       "picture" => $IMAGE_PATH . "premkumar.jpg",
-      "description" => "",
+      "description" => "
+      Howdy! I'm Premkumar Giridhar. I'm a 3rd year CS major from the Bay Area. Primarily, I'm interested in digital design (digital design is my passion), but I've done quite a bit of software work through my internships. This year, I'm a DAV lead, which means that I'm a fan of Verilog and decoding mysterious timing violations. Outside of school, I enjoy playing a ton of Mario Kart and Breath of the Wild and playing the New York Times crossword religiously. If you're interested in chip design/digital design, how I coded in Typescript for the entire summer, or how to tame the Lord of the Mountain (BOTW moment) then feel free to reach out!",
       "pronouns" => "He/Him",
     ],
     "Zayyir Ahmed" => [
@@ -207,7 +216,8 @@ $officer_data = [
       "major" => "EE",
       "class" => "3rd Year",
       "picture" => $IMAGE_PATH . "megan.jpg",
-      "description" => "",
+      "description" =>
+        "Hi everyone! I'm Megan Chen, third year EE major, and one of this year's Projects and Lab Managers! Feel free to find me in the lab and ask me anything :D",
       "pronouns" => "Any",
     ],
     "Timothy Jacques" => [
@@ -215,7 +225,8 @@ $officer_data = [
       "major" => "CE",
       "class" => "4th Year",
       "picture" => $IMAGE_PATH . "timothy.jpg",
-      "description" => "",
+      "description" =>
+        "Hello! I am one of this year's Project and Lab Managers, and am a former DAV and MM lead. I'm a fourth year computer engineer, and love anything embedded or digital design related. My hobbies include biking, hiking, making, and gaming. Feel free to ask about anything IEEE related, or just for advice in general!",
       "pronouns" => "He/Him",
     ],
     "Dominic Olson" => [
