e2ccaceae3a43d54e01cecb9737499972cd6913e
Paul Zhang <paulz@ucla.edu>
2023-11-09 18:00:22 -0800

    Add prototype outreach page

--- views/outreach.php
+++ views/outreach.php
@@ -1,39 +1,154 @@
-<!DOCTYPE HTML>
+<!DOCTYPE html>
 <html>
-<head>
+  <head>
     <title>Outreach - IEEE at UCLA</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
     <!-- JQUERY -->
-    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
     <!-- BOOTSTRAP -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+      integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+      crossorigin="anonymous"
+    />
+    <script
+      src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+      integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+      crossorigin="anonymous"
+    ></script>
 
     <!-- FONT AWESOME -->
-    <script src="https://kit.fontawesome.com/2fb39e755e.js" crossorigin="anonymous"></script>
+    <script
+      src="https://kit.fontawesome.com/2fb39e755e.js"
+      crossorigin="anonymous"
+    ></script>
 
     <!--Favicon -->
-    <link rel='icon' href='/public/favicon.ico' />
+    <link rel="icon" href="/public/favicon.ico" />
 
     <!-- STYLESHEETS -->
-    <link rel='stylesheet' href='/public/css/footer.css?v=1.0.0' />
-    <link rel='stylesheet' href='/public/css/navbar.css?v=1.0.1' />
-    <link rel='stylesheet' href='/public/css/style.css?v=1.0.0' />
+    <link rel="stylesheet" href="/public/css/footer.css?v=1.0.0" />
+    <link rel="stylesheet" href="/public/css/navbar.css?v=1.0.1" />
+    <link rel="stylesheet" href="/public/css/style.css?v=1.0.0" />
+    <link rel="stylesheet" href="/public/css/outreach.css?v=1.0.0" />
 
     <!-- SCRIPTS -->
     <script src="/public/js/about.js"></script>
+  </head>
 
-</head>
-<body>
+  <body>
     <!-- NAVBAR -->
     <?php include "./includes/navbar.html"; ?>
 
+    <div class="pt-3" />
+
     <!-- DESCRIPTION -->
-    <div class="background-ieee-blue">
-        <h1>OUTREACH</h1>
+    <div class="container-fluid text-center page-title content-wrapper">
+      <h1><span class="ieee-blue">Workshops</span> Recordings and Slides</h1>
+      <p>
+        The goal of Outreach is to support the engineering communities around us
+        through our various activities and programs such as our OPS 2.0 program,
+        community college workshop series, or helping out with various local
+        community events.
+      </p>
+      <p>
+        Want to get in contact with us? If you are a UCLA student interested in
+        joining the Outreach Committee or an outside organization inquiring
+        about a potential collaboration please contact the Outreach Coordinators
+        Nathan Nguyendinh and Eli Foerst at
+        <a href="mailto:outreach@ieeebruins.com">outreach@ieeebruins.com</a>
+      </p>
     </div>
-</body>
+
+    <div class="container-fluid ops background-ieee-blue">
+      <div class="content-wrapper">
+        <div class="row flex-container">
+          <div class="col-md-4 header">
+            <h1>OPS 2.0</h1>
+          </div>
+
+          <div class="col-md-8 descrip">
+            <p>
+              OPS 2.0 is a program dedicated to teaching IEEE student branches
+              how to run their own beginner-friendly technical project. Starting
+              in 2010, the Open Project Space (OPS) was founded by Leo Szeto at
+              UCLA with the intention of providing hands-on projects to solidify
+              the knowledge developed through ECE coursework. The OPS program is
+              designed to teach Electrical and Computer Engineering concepts to
+              fledgling engineers through hardware and software lectures,
+              worksessions and projects. Taking this to the next level, the OPS
+              2.0 program is designed to “teach the teacher” and onboard the OPS
+              program at any university focused on developing technical
+              experience for their members! Interested?
+            </p>
+            <p>
+              Learn more and sign up
+              <a href="https://openproject.space/about/#ops-at-your-school"
+                >here</a
+              >!
+            </p>
+          </div>
+        </div>
+        <div class="picture-box flex-container"></div>
+      </div>
+    </div>
+
+    <div class="container-fluid ops background-grey">
+      <div class="content-wrapper">
+        <div class="row flex-container">
+          <div class="col-md-4 header">
+            <h1>Community College Workshops</h1>
+          </div>
+
+          <div class="col-md-8 descrip">
+            <p>
+              This year, we are working with El Camino and Cerritos College to
+              organize technical workshops that teach the basics of electrical
+              engineering and design. Our main goal with these workshops is to
+              help prepare these students with the practical skills necessary to
+              succeed as they continue on into their engineering journeys.
+            </p>
+            <p>
+              If you are interested in learning more about these workshops, feel
+              free to contact us at the email provided above.
+            </p>
+          </div>
+        </div>
+        <div class="picture-box flex-container"></div>
+      </div>
+    </div>
+
+    <div class="container-fluid ops background-ieee-blue">
+      <div class="content-wrapper">
+        <div class="row flex-container">
+          <div class="col-md-4 header">
+            <h1>Outreach Events</h1>
+          </div>
+
+          <div class="col-md-8 descrip">
+            <p>
+              Another goal of the Outreach Committee is to inspire the young
+              engineers in the surrounding communities and show the many places
+              you can go within STEM. We love to be able to attend different
+              events in various communities to help spread the word about
+              engineering. Some different events that we have attended before
+              include running booths at school STEM fairs, judging and providing
+              technical assistance for hackathons, and hosting workshops at
+              surrounding schools. If you would like to get into contact with
+              IEEE at UCLA about potential collaborations, our inbox is always
+              open at
+              <a href="mailto:outreach@ieeebruins.com"
+                >outreach@ieeebruins.com</a
+              >!
+            </p>
+          </div>
+        </div>
+        <div class="picture-box flex-container"></div>
+      </div>
+    </div>
+  </body>
 </html>
