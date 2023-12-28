21e339a13dd0f04349fd65ca64feec414f5f1518
Paul Zhang <paulz@ucla.edu>
2023-12-28 01:07:45 -0800

    Fix mobile navbar jankiness

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,32 +1,23 @@
 // function to handle logic when navbar icon pressed
 // eslint-disable-next-line no-unused-vars
-function toggleNavbarIconLogic(navbarIcon) {
-  navbarIcon.classList.toggle("change");
-  toggleMobileNav();
-}
+document.querySelector(".navbar-icon").addEventListener("click", (event) => {
+  event.currentTarget.classList.toggle("change");
 
-function toggleMobileNav() {
-  var idText = "mobile-nav-list";
-  if (document.getElementById(idText).style.height === "auto") {
-    document.getElementById(idText).style.height = "0%";
-  } else {
-    document.getElementById(idText).style.height = "auto";
-  }
-}
+  const mobileNavbar = document.querySelector(".mobile-nav-list");
+  mobileNavbar.classList.toggle("open-mobile-nav-list");
+});
 
-// eslint-disable-next-line no-unused-vars
-function toggleResponsiveActive() {
-  const x = document.getElementById("responsive-content");
-  if (x.className === "responsive-dropdown-content") {
-    x.className += " active";
-  } else {
-    x.className = "responsive-dropdown-content";
-  }
+document.querySelectorAll(".dropdown-arrow").forEach((arrow) => {
+  arrow.addEventListener("click", (event) => {
+    arrow
+      .closest(".responsive-dropdown")
+      .querySelector(".responsive-dropdown-content")
+      .classList.toggle("active");
 
-  const dropdownArrow = document.getElementById("dropdown-arrow");
-  if (dropdownArrow.style.transform === "rotate(180deg)") {
-    dropdownArrow.style.transform = "none";
-  } else {
-    dropdownArrow.style.transform = "rotate(180deg)";
-  }
-}
+    if (event.currentTarget.style.transform === "rotate(180deg)") {
+      event.currentTarget.style.transform = "none";
+    } else {
+      event.currentTarget.style.transform = "rotate(180deg)";
+    }
+  });
+});
